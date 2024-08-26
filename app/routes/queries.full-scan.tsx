172e374e21da560241f81d9e77886daeceb97041
David Shaw <109576315+Batmanninja1991@users.noreply.github.com>
2024-08-25 22:15:46 -0700

    Upwork Fix
    Removed max-w-7xl
    changed to w-4/5
    makes it responsive for the viewport at 80% width.

--- app/routes/queries.full-scan.tsx
+++ app/routes/queries.full-scan.tsx
@@ -169,7 +169,7 @@ const Index = () => {
   return (
     <main className="flex-1">
       <div className="py-6">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+        <div className="w-4/5 mx-auto px-4 sm:px-6 md:px-8">
           <h1 className="text-2xl font-semibold text-green-900 py-6 dark:text-gray-100">
             FFXIV Import Search
           </h1>
